01bbb682bb4b8ace1014043cf5c38a98f770e87e
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2024-10-23 17:06:13 -0300

    Add files via upload

---
+++ modulo_2/roteamento/controllers/PessoaController.php
@@ -0,0 +1,15 @@
+<?php
+
+class PessoaController {
+    public function index() {
+        echo "Listagem de pessoas";
+    }
+
+    public function save() {
+        echo "Salvando pessoa";
+    }
+
+    public function delete() {
+        echo "Deletando pessoa";
+    }
+}
